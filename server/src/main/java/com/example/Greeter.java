3178ba9a529bfdeb80fe4ae3d6ac66c8987b575e
Naveen467 <Nrnamala@gmail.com>
2017-07-06 14:21:33 -0700

    warings have been fixed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,11 +9,14 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
+}
 
-  }
+/**
+ * @param someone the name of a person
+ * @return greeting string
+ */
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
