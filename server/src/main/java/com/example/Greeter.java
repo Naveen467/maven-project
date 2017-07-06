498d6a1f266d350bfff435d2b51b308991aceb79
Naveen467 <Nrnamala@gmail.com>
2017-07-06 14:30:47 -0700

    changes added

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -19,5 +19,7 @@ public class Greeter {
 public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
-}
+}   
+       
+
 
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -2,3 +2,7 @@
  * this is a package name.
  */
 package com.example;
+               
+
+
+
